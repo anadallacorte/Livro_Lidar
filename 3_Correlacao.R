fadb94a8a1cea8bc74d13f202787e6cd93052e95
Ana Paula Dalla Corte <73317122+anadallacorte@users.noreply.github.com>
2022-03-01 13:20:41 -0300

    Update 3_Correlacao.R

--- 3_Correlacao.R
+++ 3_Correlacao.R
@@ -24,7 +24,7 @@ require(corrplot)
 
 ########## ANALISES DAS METRICAS DAS UNIDADES AMOSTRAIS ########## -------------------------------------------------------------------------
 #Abertura da tabela com os dados biometricos e com as metricas do Lidar
-dados_bancodados<-read.csv2("C:/Livro_Lidar/Item3_Metricas/Item_3_Tabelas/DADOS_METRICAS_PARCELAS.csv", header = TRUE)
+dados_bancodados<read.csv2("C:/Livro_Lidar/Item3_Correlacao/Item_3_Tabelas/DADOS_METRICAS_PARCELAS.csv", header = TRUE)
 View(dados_bancodados)
 
 #Selecionando as variaveis que podem participar do calculo da correlacao
@@ -34,22 +34,22 @@ View(dados_cor)
 sapply(dados_cor, is.numeric)
 
 #salvando o bando de dados como "dados_pareados"
-write.csv2(dados_cor,"C:/Livro_Lidar/Item3_Metricas/Item_3_Tabelas/dados_pareados.csv")
+write.csv2(dados_cor,"C:/Livro_Lidar/Item3_Correlacao/Item_3_Tabelas/dados_pareados.csv")
 
 #Calculo das correlacoes entre as variaveis
 correlacao<-cor(dados_cor)
 View(correlacao)
 
 #Salvando a tabela de correlacao
-write.csv2(correlacao,"C:/Livro_Lidar/Item3_Metricas/Item_3_Tabelas/correlacao.csv")
+write.csv2(correlacao,"C:/Livro_Lidar/Item3_Correlacao/Item_3_Tabelas/correlacao.csv")
 
 #Construindo a matriz de correlacao entre as variaveis
 res1 <- cor.mtest(correlacao, conf.level = .05)
-jpeg("C:/Livro_Lidar/Item3_Metricas/Item_3_Imagens/correlacao.jpeg", width = 5800, height = 5000, units = "px", res = 300, quality = 5000)
+jpeg("C:/Livro_Lidar/Item3_Correlacao/Item_3_Figuras/correlacao.jpeg", width = 5800, height = 5000, units = "px", res = 300, quality = 5000)
 corrplot.mixed(correlacao, lower = "number", upper = "ellipse", lower.col = "black", 
                tl.cex=1,number.font = 2, cl.ratio = 0.2, order = "hclust", p.mat = res1$p, sig.level = .95)
 dev.off()
 
 
 #salvando a tabela de dados pareados
-write.csv2(dados_cor,"C:/Livro_Lidar/Item3_Metricas/Item_3_Tabelas/dados_pareados.csv")
+write.csv2(dados_cor,"C:/Livro_Lidar/Item3_Correlacao/Item_3_Tabelas/dados_pareados.csv")
